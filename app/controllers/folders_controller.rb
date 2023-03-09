75e1f4b4e75ea53f69cbb75bad53077a7c6c1f96
iceiceice24 <isaiah.habitan@gmail.com>
2023-03-09 14:05:48 +0800

    folder Controller updated

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,30 @@
 class FoldersController < ApplicationController
+    def index
+    end
+
+    def create
+    end
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def destory
+    end
+
+    private
+
+    def folder_params
+        params.require(:folder).permit(:folderName)
+
+    end
+
 end
