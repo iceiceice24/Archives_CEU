bc0de19ce57a77c36883891438655180fd4f83ff
iceiceice24 <isaiah.habitan@gmail.com>
2023-03-09 14:39:37 +0800

    Added views for folder and controller fix

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,30 +1,74 @@
 class FoldersController < ApplicationController
-    def index
+    def home
+        @folders = folder.all
     end
-
-    def create
-    end
-
-    def index
-    end
-
-    def new
-    end
-
-    def edit
-    end
-
-    def show
-    end
-
-    def destory
-    end
-
-    private
-
-    def folder_params
-        params.require(:folder).permit(:folderName)
-
-    end
-
-end
+   
+   def index
+      @folders = current_user.folders
+   end
+ 
+   def show
+       find_folder
+   end
+ 
+   def new
+       @folder = Folder.new 
+   end
+ 
+   def create
+        #@folder = current_user.folders.new(folder_params)
+       # @folder.user_id = current_user.id
+        @folder = folder.new(folder_params)
+      
+       if @folder.save
+           redirect_to '/folders', notice: 'Created'
+       else
+           redirect_to '/folders', notice: 'Error'
+       end
+ 
+   end
+ 
+   def edit
+       find_folder
+   end
+ 
+   def update
+       find_folder
+           if @folder.update(folder_params)
+              
+               redirect_to '/folders', notice: 'Updated'
+           else
+               render :edit
+       end
+   end
+ 
+   def destroy
+       find_folder
+       @folder.destroy
+       redirect_to '/folders', notice: 'Deleted' 
+   end
+ 
+   def remove_image
+     @folder = folder.find(params[:id])
+     @folder.image.purge
+     redirect_to @folder
+   end
+ 
+   def remove_pictures
+     @folder = folder.find(params[:id])
+     @folder.pictures.purge
+     redirect_to @folder
+   end
+ 
+ 
+   private
+ 
+   def folder_params
+       params.require(:folder).permit(:toyName, :content, :price, :email, :image, pictures: [])
+   end
+ 
+   def find_folde
+     @folder = folder.find(params[:id])
+   end
+ 
+ end
